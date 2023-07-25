0507fd41a1dab88c360c44d2617a9f0bcd8223b9
Ruben S. Garcia <rgarci12@mercadona.es>
2023-07-25 16:12:28 +0200

    feat: Declare exported functions

--- vitest.d.ts
+++ vitest.d.ts
@@ -1,16 +1,18 @@
-import type { Assertion, AsymmetricMatchersContaining } from 'vitest'
-
-type WithHost = {
-  host?: string
-}
+import { Assertion, AsymmetricMatchersContaining } from 'vitest'
+import type { RequestOptions } from './src/models'
 
 interface CustomMatchers<R = unknown> {
-  toHaveBeenFetched(options?: object): R
-  toHaveBeenFetchedWith(options?: object): R
-  toHaveBeenFetchedTimes(expectedLength?: number, options?: object): R
+  toHaveBeenFetched(options?: RequestOptions): R
+  toHaveBeenFetchedWith(options?: RequestOptions): R
+  toHaveBeenFetchedTimes(expectedLength?: number, options?: RequestOptions): R
 }
 
 declare module 'vitest' {
   interface Assertion<T = any> extends CustomMatchers<T> {}
   interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
+
+declare module 'wrapito' {
+  const matchers: CustomMatchers<any>
+  export = matchers
 }
