bafb056dde6e313a6e5ef0384a7b6ad24367f9c5
Arturo Sanchez <artsanch@mercadona.es>
2024-11-06 10:13:57 +0100

    chore: green tests

--- tests/lib/withNetwork.test.ts
+++ tests/lib/withNetwork.test.ts
@@ -1,4 +1,3 @@
-import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react'
 import { wrap, configure } from '../../src/index'
 import { vi, it, expect } from 'vitest'
